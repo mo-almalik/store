630a4d22cf8fa06fab32e5df4320ef4d8a191d07
mo-almalik <mosalahabdalla@gmail.com>
2025-02-13 12:10:17 +0200

    about us done

--- src/pages/user/AboutUs.jsx
+++ src/pages/user/AboutUs.jsx
@@ -9,8 +9,8 @@ function AboutUs() {
     <div className='container mb-10 mt-24 '>
     
 
-    <div className='grid grid-cols-1 md:grid-cols-3 gap-4 '>
-    <div class="col-span-2 flex items-center">
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3'>
+    <div class="md:col-span-1 lg:col-span-1 xl:col-span-2  flex items-center ">
 <div>
 <h3 className="text-3xl text-black mb-4 capitalize font-bold">{t("about.title")}</h3>
     <p className='text-gray-600 w-[80%]'>
@@ -30,9 +30,9 @@ function AboutUs() {
 
 </div>
     </div>
-    <div class="relative">
-        <img src={assets.about} alt='about us' className=' rounded-t-full' />
-        <div className='bg-light shadow-2xl p-5 rounded-lg absolute w-full bottom-1 md:-left-10 md:rtl:-right-10  transition-all duration-1000 '>
+    <div class="relative mt-10 md:mt-0">
+        <img src={assets.about} alt='about us' className=' rounded-t-full w-full md:w-[90%]' />
+        <div className='bg-light shadow-2xl p-5 rounded-lg absolute w-full bottom-1 left-5  md:-left-10 md:rtl:-right-10  transition-all duration-1000 '>
             <div className='flex items-center justify-between'>
                 <div className='flex items-center gap-x-1.5'><LuStar className='text-main size-6' /> <strong>{t('about.quality')}</strong></div>
                 <div className='flex items-center gap-x-1.5'><LuShieldCheck className='text-main size-6' /> <strong>{t('about.security')}</strong></div>
